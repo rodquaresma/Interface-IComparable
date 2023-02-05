f3841f556991a793545d7dfe23d89079f153331b
Rodrigo Quaresma <rodrigo.quaresma.souza@gmail.com>
2023-02-05 13:13:18 -0300

    Implementação da Interface IComparable

---
+++ Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Course2.Entities
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string list)
+        {
+            string[] vect = list.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Argument is not a Employee");
+            }
+            Employee other = (Employee)obj;
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Course2.Entities;
+
+namespace Course2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\rodri\OneDrive\Desktop\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                        Console.WriteLine(emp);                    
+                }                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error in file path: " + e.Message);
+            }
+
+
+        }
+    }
+}        
+        
